0f2d54d0bcb39418792dd93fec6df55baf22821b
Arhan Chaudhary <arhan.ch@gmail.com>
2024-05-04 02:47:00 -0400

    First blog post >:)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import robotsTxt from "astro-robots-txt";
-
 import sitemap from "@astrojs/sitemap";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://arhan.sh',
-  integrations: [robotsTxt(), sitemap()]
+  integrations: [robotsTxt(), sitemap(), mdx()]
 });
