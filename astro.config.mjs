0833c73242a6efad40fa6732ddedcf0a2790e745
Arhan Chaudhary <arhan.ch@gmail.com>
2024-07-04 11:48:24 -0700

    (blog): table of contents

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import remarkToc from "remark-toc";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
 
@@ -9,8 +10,9 @@ export default defineConfig({
   site: "https://arhan.sh",
   integrations: [robotsTxt(), sitemap(), mdx()],
   markdown: {
+    remarkPlugins: [remarkToc],
     shikiConfig: {
-      theme: 'one-dark-pro',
-    }
-  }
+      theme: "one-dark-pro",
+    },
+  },
 });
